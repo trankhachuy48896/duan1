60d162a5ac90ab7fbcfaead7aa8dd68e742cfae1
emptyuser1102 <giangtdph48875@gmail.com>
2024-11-23 23:44:36 +0700

    js register

--- views/sign_up.php
+++ views/sign_up.php
@@ -2,33 +2,36 @@
 include "header.php";
 ?>
 <div class="form">
-    <div class="form_login">
+    <div class="register">
         <h2>Đăng Ký</h2>
-        <form action="">
+        <form action="" id="register_form" method="post">
             Họ tên:
-            <input type="text" name="" id="" placeholder="Nhập họ tên" require>
+            <input type="text" name="" id="name_register" placeholder="Nhập họ tên" require>
 
             Địa chỉ email:
-            <input type="email" name="" id="" placeholder="Nhập email" require>
+            <input type="email" name="" id="email_register" placeholder="Nhập email" require>
 
             Tên đăng nhập:
-            <input type="text" name="" id="" placeholder="Nhập tên đăng nhập" require>
+            <input type="text" name="" id="username_register" placeholder="Nhập tên đăng nhập" require>
 
             Mật khẩu:
-            <input type="password" name="" id="" placeholder="Nhập mật khẩu" require>
+            <input type="password" name="" id="password_register" placeholder="Nhập mật khẩu" require>
 
             Xác nhận mật khẩu:
-            <input type="password" name="" id="" placeholder="Xác nhận mật khẩu" require>
+            <input type="password" name="" id="verify_password" placeholder="Xác nhận mật khẩu" require>
 
             Số điện thoại:
-            <input type="text" name="" id="" placeholder="Nhập số điện thoại" require>
+            <input type="text" name="" id="phoneNumer_register" placeholder="Nhập số điện thoại" require>
             
-            <button>Đăng nhập</button>
+            <button type="submit">Đăng nhập</button>
+
+            <br>
+            <span id="error-message" style="color: red;"></span>
         </form>
     </div>
 
 </div>
-
+<script src="javaRegister.js"></script>
 <?php
 include "footer.php";
 ?>
